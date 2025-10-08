aa094399a7cbe3dfa2db0d549bfed7d628cd3229
leomarjms26-netizen <leomar.jms26@gmail.com>
2025-10-08 13:56:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if buscar and entrada:
 
         if filtro.empty:
             st.error(
-            f"❌ Nenhuma Porta disponível encontrada para: {entrada}  \n"
+            f"❌ Nenhuma Porta disponível encontrada para: \n{entrada}  \n"
             f"📞 Ligue para o TI para Atualizar a Caixa: (11) 94484-7040 ou Clique no Ícone do Whatsapp para ser redirecionado"
             )
             
@@ -74,6 +74,7 @@ if buscar and entrada:
             
             # Mostra no Streamlit
             st.table(df_sem_indice)
+
 
 
 
