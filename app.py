5721f8b05c065713cedb71313774819658831d7b
leomarjms26-netizen <leomar.jms26@gmail.com>
2025-10-08 10:42:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Verificador de Portas Disponíveis")
+
+entrada = st.text_input("Digite o identificador (ex: CB07-SP06-CX15)")
+
+if entrada:
+    try:
+        cabo_val, primaria_val, caixa_val = [x.strip() for x in entrada.split("-")]
+    except ValueError:
+        st.error("❌ Formato inválido. Use: CB01-SP01-CX01")
+    else:
+        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRrExQYNSa64iJs2DK5MrnUhsXSrPhrxqYswB1zTRYCoGlAw9gMqh5d5G5SMaDaJtDd78gy4Ud-UHFW/pub?output=xlsx"
+        df = pd.read_excel(url)
+        df.columns = [
+            "CABO", "PRIMARIA", "CAIXA", "ID", "PORTA", "CAPACIDADE",
+            "INTERFACE", "DATA_DE_ATUALIZACAO", "OCUPADA", "OBSERVACAO"
+        ]
+
+        filtro = df[
+            (df["CABO"].astype(str).str.upper().str.strip() == cabo_val.upper()) &
+            (df["PRIMARIA"].astype(str).str.upper().str.strip() == primaria_val.upper()) &
+            (df["CAIXA"].astype(str).str.upper().str.strip() == caixa_val.upper()) &
+            (df["OCUPADA"].astype(str).str.upper().str.strip() == "NÃO")
+        ]
+
+        if filtro.empty:
+            st.error(f"❌ Nenhuma Porta disponível encontrada para: {entrada}\n📞 Ligue para o TI para Atualizar a Caixa: (11) 94484-7040")
+        else:
+            st.success(f"🟢 Portas Disponíveis para: {entrada}")
+            st.dataframe(filtro)
